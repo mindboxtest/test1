b59208da04f950b5924b94eea2bdd7586de217ec
mindboxtest <44244371+mindboxtest@users.noreply.github.com>
2018-10-17 23:22:25 +0300

    Add files via upload

---
+++ CalcArea/CalcArea.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcArea
+{
+	/// <summary>
+	/// Фигура (базовый)
+	/// </summary>
+    public abstract class Figure
+    {
+        /// <summary>
+		/// Расчет площади фигуры
+		/// </summary>
+		/// <returns>Возвращает площадь</returns>
+        public abstract double Area();
+    }
+    /// <summary>
+    /// Треугольник
+    /// </summary>
+    public class Triangle : Figure
+    {
+        /// <summary>
+        /// Сторона A
+        /// </summary>
+        public double A { private get; set; }
+
+        /// <summary>
+        /// Сторона B
+        /// </summary>
+        public double B { private get; set; }
+
+        /// <summary>
+        /// Сторона C
+        /// </summary>
+        public double C { private get; set; }
+
+        /// <summary>
+        /// Треугольник
+        /// </summary>
+        /// <param name="a">Сторона a</param>
+        /// <param name="b">Сторона b</param>
+        /// <param name="c">Сторона c</param>
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        /// <summary>
+        /// Проверка - является ли треугольник прямоугольным
+        /// </summary>
+        /// <returns>True or False</returns>
+        public bool Check()
+        {
+            return A * A == B * B + C * C || B * B == A * A + C * C || C * C == A * A + B * B;
+        }
+
+        /// <summary>
+        /// Площадь треугольника
+        /// </summary>
+        /// <returns>Площадь треугольника</returns>
+        public override double Area()
+        {
+            double P = (A + B + C) / 2;
+            return Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+        }
+    }
+    /// <summary>
+    /// Круг
+    /// </summary>
+    public class Around : Figure
+    {
+		/// <summary>
+		/// Радиус
+		/// </summary>
+        public double R { private get; set; }
+
+		/// <summary>
+		/// Круг
+		/// </summary>
+		/// <param name="r">Радиус</param>
+		public Around(double r)
+        {
+            R = r;
+        }
+
+        /// <summary>
+        /// Площадь круга
+        /// </summary>
+        /// <returns>Площадь круга</returns>
+        public override double Area()
+        {
+            return Math.PI * R * R;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcArea;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			List<Figure> figures = new List<Figure>();
+
+			figures.Add(new Triangle(2, 9, 8));
+			figures.Add(new Triangle(2, 10, 8));
+			figures.Add(new Triangle(2, 15, 15));
+			figures.Add(new Around(5));
+			figures.Add(new Around(20));
+
+			double sunArea = 0.0;
+
+			foreach (var it in figures)
+				sunArea += it.Area();
+        
+			Console.WriteLine($"Суммарная площадь всех фигур: {sunArea}");
+
+            Console.ReadLine();
+        }
+    }
+}
